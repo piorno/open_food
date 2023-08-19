443b31583b5390b4bbbf0d057308ee0c1e8b2089
piorno <70115275+piorno@users.noreply.github.com>
2023-08-19 18:17:09 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,15 @@
+#Projeto Api open food facts
+
+O projeto tem um CRON que valida todo dia as 03:00 gmt -3 a api aperta do open fact foods para manter a base de dados atualizada
+com metodos put para atualizar os dados, get para ver tudo com paginação o get por codigo e delete
+
+Liguagem usada Node.js com typescript com o framework express
+
+
+para intalar o projeto
+entre no diretorio do projeto e rode o comando: 
+> docker-compose up -d --build
+
+
+
+>  This is a challenge by [Coodesh](https://coodesh.com/)
---
+++ src/server.ts
@@ -0,0 +1,47 @@
+import dotenv from 'dotenv';
+import ProductsController from './controllers/productsController';
+import { Logs } from './models/logs';
+import { up } from './seeds/productsSeeds';
+const productsController = new ProductsController();
+dotenv.config();
+
+const PORT = parseInt(`${process.env.PORT || 3000}`);
+
+import app from './app';
+import mongoose from 'mongoose';
+// const mongoose = require('mongoose');
+
+up();
+
+console.log(`${process.env.MONGO_HOST}${process.env.MONGO_DATABASE}`);
+
+
+mongoose.connect(`${process.env.MONGO_HOST}${process.env.MONGO_DATABASE}`, {
+    // useNewUrlParser: true, useUnifiedTopology: true
+})
+
+const schedule = require('node-schedule');
+
+const job = schedule.scheduleJob('0 0 3 * * *', async function () {
+    console.log('aqui');
+    
+    const startDate = new Date();
+    const {endDate, totalMemory} = await productsController.scheduleProducts();
+    var seconds = (endDate.getTime() - startDate.getTime()) / 1000;
+    let log = {
+        executed: startDate,
+        memory: totalMemory,
+        time: Math.ceil(seconds)
+    }
+    
+    Logs.create(log)
+});
+
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+    console.log("Connected successfully");
+});
+
+app.listen(PORT, () => console.log(`Server is running at ${PORT}.`));
